8ca59726e02a2e189bb84c2a9f9a732724b032dc
Tanvi Rayakwar <tanvirayakwar@gmail.com>
2026-08-13 19:53:31 +0530

    program9

---
+++ greatest.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+    int a,b,c;
+    printf("Enter three numbers:");
+    scanf("%d %d %d",&a,&b,&c);
+    if (a>b && a>c) {
+        printf("%d is greater number",a);
+    }
+    else if (b>a && b>c) {
+        printf("%d is greater",b);
+    }
+    else {
+        printf("%d is greater",c);
+    }
+    return 0;
+
+}
