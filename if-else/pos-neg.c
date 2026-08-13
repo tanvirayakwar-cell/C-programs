7690b0006d68bb370d4aa2a75d5aaccca05ea444
Tanvi Rayakwar <tanvirayakwar@gmail.com>
2026-08-13 20:02:10 +0530

    program1

---
+++ if-else/pos-neg.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    if (num>0) {
+        printf("%d is a positive number",num);
+    }
+    else if (num<0) {
+        printf("%d is a negative number",num);
+    }
+    else {
+        printf("number is zero");
+    }
+    return 0;
+}
